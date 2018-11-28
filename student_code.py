9cd618a8f7584350b06b6c8fc52d9d223d7c2489
Zuohao She <zuohaoshe2013@u.northwestern.edu>
2018-11-28 15:19:22 -0600

    First commit

---
+++ student_code.py
@@ -0,0 +1,15 @@
+import math
+import re
+
+class Bayes_Classifier:
+
+    def __init__(self):
+        pass
+
+
+    def train(self, lines):
+        pass
+
+
+    def classify(self, lines):
+        pass
